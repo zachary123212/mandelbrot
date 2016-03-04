720c61406b6a24606f56df4f69e250c126308cd0
zachary123212 <zlindenbaum@gmail.com>
2016-03-04 18:16:18 -0500

    made structure for Mandelbrot object

--- sketch.js
+++ sketch.js
@@ -1,10 +1,19 @@
 var DIMX = 700;
 var DIMY = 700;
 
+var mand
+
+function Mandelbrot() {
+  // show = function() {
+  //   for(i = 0; i < )
+  // };
+}
+
 function setup() {
   createCanvas(DIMX, DIMY);
+  mand = new Mandelbrot();
 }
 
 function draw() {
-
+  mand.show();
 }
