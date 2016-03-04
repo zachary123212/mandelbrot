1b431c95330eb366f5aff0fa4d9d5f09eafbe3a6
zachary123212 <zlindenbaum@gmail.com>
2016-03-04 18:29:02 -0500

    added functions for arbitrary complex plane

--- sketch.js
+++ sketch.js
@@ -3,6 +3,17 @@ var DIMY = 700;
 
 var mand;
 
+function ComplexPlane(){
+  this.xMax =  1 * (DIMX / 2);
+  this.xMin = -1 * (DIMX / 2);
+  this.yMax = -1 * (DIMY / 2);
+  this.yMin =  1 * (DIMX / 2);
+
+  this.plot = function(x,y){
+    rect(DIMX/2 + x, DIMY/2 - y, 1, 1);
+  };
+}
+
 function Mandelbrot() {
 
   this.show = function(){
@@ -14,9 +25,12 @@ function Mandelbrot() {
 
 function setup() {
   createCanvas(DIMX, DIMY);
+  plane = new ComplexPlane();
   mand = new Mandelbrot();
 }
 
 function draw() {
+  plane.plot(0,0);
+  plane.plot(10,20);
   mand.show();
 }
